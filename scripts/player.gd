923d3ffdd529e3c1cea7981aec07e3b244101b5f
Guillaume Vilar <guillaume.vilar@arista.com>
2023-08-11 10:59:35 +0100

    [JOYSTICK]: Fix player movement to support joystick

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,7 +9,8 @@ const CHARACTER_INDEX_TO_PATH = {
 }
 
 puppet var puppet_pos = Vector2()
-puppet var puppet_motion = Vector2()
+puppet var puppet_velocity = Vector2()
+puppet var puppet_anim = "standing"
 
 export var stunned = false
 
@@ -31,23 +32,23 @@ func _ready():
 
 
 func _physics_process(_delta):
-	var motion = Vector2()
+	var direction = Vector2.ZERO
+	var velocity = Vector2()
+	var new_anim = "standing"
 
 	if is_network_master() && current_health >= 1:
-		if Input.is_action_pressed("move_left"):
-			motion += Vector2(-1, 0)
-		if Input.is_action_pressed("move_right"):
-			motion += Vector2(1, 0)
-		if Input.is_action_pressed("move_up"):
-			motion += Vector2(0, -1)
-		if Input.is_action_pressed("move_down"):
-			motion += Vector2(0, 1)
-
+		direction.x =  Input.get_axis("move_left", "move_right")
+		direction.y = Input.get_axis("move_up", "move_down")
 		var bombing = Input.is_action_pressed("set_bomb")
 
 		if stunned:
 			bombing = false
-			motion = Vector2()
+			direction = Vector2.ZERO
+		if direction != Vector2.ZERO:
+			direction = direction.normalized()
+
+		velocity.x = direction.x * MOTION_SPEED
+		velocity.y = direction.y * MOTION_SPEED
 
 		if bombing and not prev_bombing:
 			var bomb_name = String(get_name()) + str(bomb_index)
@@ -56,21 +57,28 @@ func _physics_process(_delta):
 
 		prev_bombing = bombing
 
-		rset("puppet_motion", motion)
+		# Animation part
+		new_anim = "standing"
+		if direction != Vector2.ZERO:
+			var value = velocity.y
+			if velocity.y < 0:
+				new_anim = "walk_up"
+			elif velocity.y > 0:
+				new_anim = "walk_down"
+			if abs(velocity.x) > abs(velocity.y):
+				if velocity.x < 0:
+					new_anim = "walk_left"
+				else:
+					new_anim = "walk_right"
+
+		# Setting variables on puppets
+		rset("puppet_velocity", velocity)
 		rset("puppet_pos", position)
+		rset("puppet_anim", new_anim)
 	else:
 		position = puppet_pos
-		motion = puppet_motion
-
-	var new_anim = "standing"
-	if motion.y < 0:
-		new_anim = "walk_up"
-	elif motion.y > 0:
-		new_anim = "walk_down"
-	elif motion.x < 0:
-		new_anim = "walk_left"
-	elif motion.x > 0:
-		new_anim = "walk_right"
+		velocity = puppet_velocity
+		new_anim = puppet_anim
 
 	if stunned:
 		new_anim = "stunned"
@@ -80,7 +88,7 @@ func _physics_process(_delta):
 		get_node("anim").play(current_anim)
 
 	# FIXME: Use move_and_slide
-	move_and_slide(motion * MOTION_SPEED)
+	move_and_slide(velocity)
 	if not is_network_master():
 		puppet_pos = position # To avoid jitter
 
